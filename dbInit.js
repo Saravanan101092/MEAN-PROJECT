30b5e45f9e50e56fbd8b2f86b9840550899b1b42
saravanan101092 <saravanan101092@gmail.com>
2017-04-28 09:06:35 +0530

    Cloud mongo db

--- dbInit.js
+++ dbInit.js
@@ -6,7 +6,7 @@ var MongoClient = require('mongodb').MongoClient;
 var dbConnection = module.exports = function(app){
 	app.connect= function(cb) {
 		console.log("connecting..");
-		MongoClient.connect('mongodb://localhost:27017/warDB', function(err, db) {
+		MongoClient.connect('mongodb://saru101092:Saru%402017@cluster0-shard-00-00-ld6l0.mongodb.net:27017,cluster0-shard-00-01-ld6l0.mongodb.net:27017,cluster0-shard-00-02-ld6l0.mongodb.net:27017/warDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', function(err, db) {
 			if (err) {
 				return console.log("Error: " + err);
 			}
